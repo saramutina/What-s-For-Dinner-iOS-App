cdeda90b41f390a7d1dd818e833c0ca6b7d6bb9e
Katie Saramutina <saramutina@gmail.com>
2022-04-17 02:43:07 +0300

    Initial Commit

---
+++ What's For Dinner/What_s_For_DinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  What_s_For_DinnerApp.swift
+//  What's For Dinner
+//
+//  Created by Katie Saramutina on 17.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct What_s_For_DinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
